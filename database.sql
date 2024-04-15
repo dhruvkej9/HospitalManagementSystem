dee428297f6b05c2efa4ca03d0a82fbbdd6f749d
dhruvkej9 <dhruvkejri9@gmail.com>
2024-04-15 10:51:13 +0530

    Some Updates

--- database.sql
+++ database.sql
@@ -246,6 +246,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (27,'dhruvkejri912','12345678','darasingh@yahoo.in'),(28,'rahul','12345678','dg@6181.com'),(29,'harry','12345678','harry@codewithharry.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
